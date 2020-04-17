2474ae8671ace6ddd55f23f233decd1accd7d798
Brigittb <brigittb@stud.ntnu.no>
2020-04-17 20:58:18 +0200

    index,test

---
+++ tests/serverTest.js
@@ -0,0 +1,24 @@
+let axios = require('axios');
+let appListen = require('./server').appListen;
+//import { appListen } from './server';
+
+axios.defaults.baseURL = 'http://localhost:3000';
+
+beforeAll(() => appListen);
+
+describe('App responds on requests', () => {
+    it('GET / returns 200', done => {
+        axios.get('/').then(response => {
+            expect(response.status).toEqual(200);
+            done();
+        });
+    });
+    it('PUT / returns 404', done => {
+        axios.put('/').catch((error) => {
+            if (!error.response) throw new Error();
+
+            expect(error.response.status).toEqual(404);
+            done();
+        });
+    });
+});
